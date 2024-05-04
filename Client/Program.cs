9e92fe983f2f8820f0b0ba1485efed42d00988bc
minutemancarlo <67291699+minutemancarlo@users.noreply.github.com>
2024-05-05 03:42:34 +0800

    Added Principal Claims

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,16 +1,23 @@
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using NCMS_wasm.Client;
+using NCMS_wasm.Client.Shared;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("API", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+       .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
 builder.Services.AddOidcAuthentication(options =>
 {
     builder.Configuration.Bind("Auth0", options.ProviderOptions);
-    options.ProviderOptions.ResponseType = "code";
-});
+    options.ProviderOptions.ResponseType = "code";    
+}).AddAccountClaimsPrincipalFactory<ArrayClaimsPrincipalFactory<RemoteUserAccount>>();
+
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
