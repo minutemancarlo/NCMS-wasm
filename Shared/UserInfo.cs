8e1df58d33647fd0f186f53fc782ab85c741a416
minutemancarlo <67291699+minutemancarlo@users.noreply.github.com>
2024-05-07 02:50:22 +0800

    Added Auth views and user management update

--- Shared/UserInfo.cs
+++ Shared/UserInfo.cs
@@ -6,23 +6,23 @@ using System.Threading.Tasks;
 
 namespace NCMS_wasm.Shared
 {
-    public class UserInfo
+    public class UserInfo : SetUserRole
     {
         public string Email { get; set; }
 
-        public bool Blocked { get; set; }
+        public bool Blocked { get; set; } = false;
 
-        public bool EmailVerified { get; set; }
+        public bool EmailVerified { get; set; } = false;
 
         public string GivenName { get; set; }
 
         public string FamilyName { get; set; }
 
-        public string Connection { get; set; }
+        public string Connection { get; set; } = "con_V6mgoPIOAel8SAtD";
 
         public string Password { get; set; }
 
-        public bool VerifyEmail { get; set; }
+        public bool VerifyEmail { get; set; } = true;
     }
 
 }
