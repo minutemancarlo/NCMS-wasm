47f5f417c4b941c600dca8d0798c8f0fed83aa1e
minutemancarlo <67291699+minutemancarlo@users.noreply.github.com>
2024-05-09 08:53:49 +0800

    Added guest placeholders

---
+++ Shared/Base.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCMS_wasm.Shared
+{
+    public class BaseModel
+    {        
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime UpdatedOn { get; set; }
+    }
+}
---
+++ Shared/GuestsInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCMS_wasm.Shared
+{
+    public class GuestsInfo : BaseModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime? CheckInDate { get; set; }
+        public DateTime? CheckOutDate { get; set; }
+        public DateTime? ArrivalDate { get; set; }
+        public int Rooms { get; set; }
+        public int Children { get; set; }
+        public int Adults { get; set; }
+        public string GuestType { get; set; }
+    }
+}
