7bda0dc84ff8eb30a9965049eaf03a99fe09ba1e
minutemancarlo <carloboado18@gmail.com>
2024-09-03 00:06:24 +0800

    Added RDLC Report Template for Web Server

---
+++ Server/Controllers/ReportController.cs
@@ -0,0 +1,61 @@
+﻿using AspNetCore.Reporting;
+using Microsoft.AspNetCore.Mvc;
+using NCMS_wasm.Client.Pages.HR;
+using NCMS_wasm.Server.Services;
+using System.Data;
+using System.Text;
+
+namespace NCMS_wasm.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly EmployeeService _employeeService = new EmployeeService();
+        public ReportController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            System.Text.Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        }
+
+        [HttpGet("GetReport")]        
+        public IActionResult GetReport(int reportType)
+        {
+            var dt = new DataTable();
+            dt = _employeeService.GetEmployees();
+            string mimeType = "";
+            int extenstion = 1;
+            //var path = $"{this._webHostEnvironment.WebRootPath}\\Reports\\Report1.rdlc";
+            //var path =Path.Combine(_webHostEnvironment.WebRootPath, "Reports", "Report1.rdlc");
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "wwwroot", "Reports", "Report1.rdlc");
+
+            if (!System.IO.File.Exists(path))
+            {
+                Console.WriteLine("File not found.");
+                return NotFound("Report file not found.");
+            }
+            else
+            {
+                Console.WriteLine("File exists");
+            }
+            Dictionary<string, string> parameter = new Dictionary<string, string>();
+            parameter.Add("param", "Sample Report");
+            LocalReport localReport = new(path);            
+            localReport.AddDataSource(dataSetName:"dsEmployee", dt);
+
+            if(reportType == 1)
+            {
+                var result = localReport.Execute(RenderType.Pdf, extenstion, parameter, mimeType);
+                return File(result.MainStream, "application/pdf");
+            }
+            else
+            {
+                var result = localReport.Execute(RenderType.Excel, extenstion, parameter, mimeType);
+                return File(result.MainStream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+            }
+
+        }
+
+    }
+}
---
+++ Server/Services/EmployeeService.cs
@@ -0,0 +1,30 @@
+﻿using System.Data;
+
+namespace NCMS_wasm.Server.Services
+{
+    public class EmployeeService
+    {
+        public DataTable GetEmployees()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Id");
+            dt.Columns.Add("Name");
+            dt.Columns.Add("Department");
+            dt.Columns.Add("Designation");
+            dt.Columns.Add("Salary");
+
+            dt.Rows.Add(1, "John", "IT", "Manager", 5000);
+            dt.Rows.Add(2, "Smith", "HR", "Executive", 3000);
+            dt.Rows.Add(3, "Mark", "IT", "Developer", 4000);
+            dt.Rows.Add(4, "Mary", "HR", "Manager", 5000);
+            dt.Rows.Add(5, "Sara", "IT", "Developer", 4000);
+            dt.Rows.Add(6, "David", "IT", "Developer", 4000);
+            dt.Rows.Add(7, "Peter", "HR", "Executive", 3000);
+            dt.Rows.Add(8, "Julie", "HR", "Manager", 5000);
+            dt.Rows.Add(9, "Tom", "IT", "Developer", 4000);
+            dt.Rows.Add(10, "Jerry", "IT", "Developer", 4000);
+
+            return dt;
+        }
+    }
+}
