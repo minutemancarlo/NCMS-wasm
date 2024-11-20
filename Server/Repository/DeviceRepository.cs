5c54d587a75fed909fe4932d7168eb47d127993a
Anubis <carloboado18@gmail.com>
2024-11-21 00:03:39 +0800

    Added Leak Warning

--- Server/Repository/DeviceRepository.cs
+++ Server/Repository/DeviceRepository.cs
@@ -17,7 +17,12 @@ namespace NCMS_wasm.Server.Repository
             string query = "SELECT * FROM Devices";
             return await _dbConnection.QueryAsync<Device>(query);
         }
-
+        public async Task<bool> GetLeakResult()
+        {
+            string query = "SELECT DeviceValue FROM Devices Where DeviceType=1";
+            var res = await _dbConnection.ExecuteScalarAsync<int>(query);
+            return res < 750;
+        }
         public async Task<int> AddDeviceAsync(Device device)
         {
             var parameters = new DynamicParameters();
