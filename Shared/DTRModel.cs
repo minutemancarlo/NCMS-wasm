2ccb9bc86bee5d2dbdf873a5688670fd45ae3e1b
Anubis <carloboado18@gmail.com>
2024-11-26 23:44:00 +0800

    Added DTR Page and Layout

---
+++ Shared/DTRModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCMS_wasm.Shared
+{
+    public class DTRModel
+    {
+        public int EmployeeId { get; set; }
+        public string? EmployeeName { get; set; }
+        public DateTime? CutOffDate { get; set; }
+        public DateTime? TimeIn { get; set; }
+        public DateTime? TimeOut { get; set; }
+    }
+}
